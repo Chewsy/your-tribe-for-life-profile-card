53d6b730900274c0629411ac682ea8adcb2d2cdc
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-13 19:12:57 +0200

    #4 ✨feat: Component NameField.svelte

---
+++ src/lib/NameField.svelte
@@ -0,0 +1,41 @@
+<script>
+    let { chama } = $props();
+    const letters = $derived(chama.name.toUpperCase().split(""));
+</script>
+
+<div class="name-field">
+    <p>Volledige naam:</p>
+
+    <div class="letters">
+        {#each letters as letter}
+            <span class="letter">{letter === " " ? "\u00A0" : letter}</span>
+        {/each}
+    </div>
+</div>
+
+<style>
+    p {
+        padding: 0;
+        margin: 0;
+    }
+
+    .name-field {
+        display: flex;
+        flex-direction: row;
+        gap: 3.125em;
+    }
+
+    .letters {
+        display: flex;
+        gap: 0.625em;
+
+        & .letter {
+            margin: 0;
+            padding: 0;
+            background-color: hsl(0, 0%, 100%);
+            aspect-ratio: 1 / 1;
+            width: 1.25em;
+            text-align: center;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import NameField from "$lib/NameField.svelte";
+
+    let { data } = $props();
+    const chama = $derived(data.chama);
+    const mugshot = $derived(`https://fdnd.directus.app/assets/${chama.mugshot}`);
+</script>
+
+<main>
+    <img src={mugshot} alt={`Foto van ${data.chama.name}`} width="100px" />
+    <NameField chama={data.chama} />
+</main>
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    /* ! Tijdelijk ! */
+    main {
+        width: 100dvw;
+        height: 100dvh;
+        background-color: gray;
+    }
+</style>
