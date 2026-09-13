0dda3bee9a7045ed3c822cd47a4c26df4aa29c9d
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-13 20:52:30 +0200

    #2 💄style: h1 scoped styling added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '$lib/styleguide.css';
+    import "$lib/styleguide.css";
 
     import NameField from "$lib/NameField.svelte";
     import TextField from "$lib/TextField.svelte";
@@ -14,16 +14,22 @@
 <main>
     <!-- <img src={mugshot} alt={`Foto van ${data.chama.name}`} width="100px" /> -->
 
+    <h1>Visitekaartje</h1>
     <NameField text={chama.name} label="Volledige naam:" />
     <NameField text={chama.nickname} label="Nickname:" />
-
     <TextField text={chama.birthdate} label="Geboortedatum:" />
 </main>
 
 <style>
-main{
-    display: flex;
-    flex-direction: column;
-    gap: var(--default-spacing);
-}
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: var(--default-spacing);
+    }
+
+    h1 {
+        padding-bottom: var(--small-spacing);
+        border-bottom: 2px solid var(--dark-blue);
+        margin-bottom: var(--large-spacing);
+    }
 </style>
