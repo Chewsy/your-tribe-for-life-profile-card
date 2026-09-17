32977e641e3aa2344633ad853f3a9311d22417ca
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-09-17 19:06:18 +0200

    #2 💄style: Skeuomorphism background-image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,11 +65,17 @@
         flex-direction: column;
         gap: var(--default-spacing);
 
-        @media (width >= 1150px) {
+        @media (width >= 1200px) {
+            background-color: var(--baby-blue);
             border: thick double var(--dark-blue);
             padding: var(--large-spacing);
             border-radius: 10px;
-            box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            box-shadow:
+                rgba(0, 0, 0, 0.09) 0px 2px 1px,
+                rgba(0, 0, 0, 0.09) 0px 4px 2px,
+                rgba(0, 0, 0, 0.09) 0px 8px 4px,
+                rgba(0, 0, 0, 0.09) 0px 16px 8px,
+                rgba(0, 0, 0, 0.09) 0px 32px 16px;
         }
     }
 
